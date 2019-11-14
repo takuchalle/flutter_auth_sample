f5f42631571f51437bf4eb6dc6ea9a8845170c75
Takuya OHASHI <tky.ohashi@gmail.com>
2019-11-15 07:59:34 +0900

    Add Authenticator Provider to top of Widget tree

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import 'app.dart';
+import 'auth/authenticator.dart';
 
-void main() => runApp(App());
+void main() => runApp(
+      Provider<Authenticator>(
+        builder: (_) => Authenticator(),
+        child: const App(),
+      ),
+    );
