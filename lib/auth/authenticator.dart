cae6e69515107bf465aebc6e38bba90e15d69cc8
Takuya OHASHI <tky.ohashi@gmail.com>
2019-11-15 22:57:59 +0900

    Use firebase_auth to login

--- lib/auth/authenticator.dart
+++ lib/auth/authenticator.dart
@@ -1,6 +1,16 @@
 import 'dart:async';
 
+import 'package:firebase_auth/firebase_auth.dart';
+
 class Authenticator {
-  Future<void> signIn() async {}
-  Future<void> signOut() async {}
+  Future<void> signIn() async {
+    await _auth.signInAnonymously();
+  }
+
+  Future<void> signOut() async {
+    await _auth.signOut();
+  }
+
+  final _auth = FirebaseAuth.instance;
+  Stream<FirebaseUser> get onAuthStateChanged => _auth.onAuthStateChanged;
 }
