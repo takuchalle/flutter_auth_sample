ed36b415a30a46140dbd7980842ec5e8c02b8dca
Takuya OHASHI <tky.ohashi@gmail.com>
2019-11-18 22:12:09 +0900

    Add DotPaginationSwiper

---
+++ lib/widgets/color_dot.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+@immutable
+class ColorDot extends StatelessWidget {
+  const ColorDot({Key key, this.color, this.radius}) : super(key: key);
+
+  final Color color;
+  final double radius;
+
+  @override
+  Widget build(BuildContext context) {
+    Color color = this.color ?? Theme.of(context).primaryColor;
+    double radius = this.radius ?? 5;
+
+    return CircleAvatar(
+      foregroundColor: color,
+      radius: radius,
+    );
+  }
+}
