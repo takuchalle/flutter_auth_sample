972e09dd32d6aa5605c9f01868f2e57a02b9848d
Takuya OHASHI <tky.ohashi@gmail.com>
2019-12-16 22:39:28 +0900

    add user model

--- lib/models/models.dart
+++ lib/models/models.dart
@@ -1,2 +1,3 @@
 export 'account.dart';
 export 'account_notifier.dart';
+export 'user.dart';
---
+++ lib/models/user.dart
@@ -0,0 +1,32 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flamingo/flamingo.dart';
+
+class User extends Document<User> {
+  User({
+    String id,
+    DocumentSnapshot snapshot,
+    Map<String, dynamic> values,
+  }) : super(id: id, snapshot: snapshot, values: values);
+
+  String name;
+
+  // For save data
+  @override
+  Map<String, dynamic> toData() {
+    final data = <String, dynamic>{};
+    writeNotNull(data, 'name', name);
+    return data;
+  }
+
+  // For load data
+  @override
+  void fromData(Map<String, dynamic> data) {
+    name = valueFromKey<String>(data, 'name');
+  }
+
+  // Call after create, update, delete.
+  @override
+  void onCompleted(ExecuteType executeType) {
+    print('$executeType');
+  }
+}
